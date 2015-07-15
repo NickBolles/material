2cbf58f9512ffabd3c36d46acc3bd9139bdf97e3
sup3rb0wlz <nickbolles@live.com>
2015-07-14 23:19:40 -0500

    init

---
+++ docs/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.3.15",
+  "version": "0.10.1-rc1",
+  "repository": "https://github.com/angular/material",
+  "commit": "ee1fed1954ef34e83bad0ae01e3b10d3f3166173",
+  "date": ""
+});
