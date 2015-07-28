c65b09a091d2106582539f69c697129044ed9654
NickBolles <nickbolles@live.com>
2015-07-28 18:01:38 -0500

    Update demo

--- dist/demos/toolbar/demoScrollFade/script.js
+++ dist/demos/toolbar/demoScrollFade/script.js
@@ -1,6 +1,17 @@
 var app = angular.module('toolbarDemo3', ['ngMaterial']);
 
 app.controller('AppCtrl', function($scope) {
+  $scope.toolbarCondensed = false;
+
+  //listen for the toolbar to be condensed, then set the scope variable
+  $scope.$on('$mdToolbarCondensed', function(toolbarElement){
+    $scope.toolbarCondensed = true;
+  });
+  //listen for the toolbar to be expanded, then set the scope variable
+  $scope.$on('$mdToolbarExpanded', function(toolbarElement){
+    $scope.toolbarCondensed = false;
+  });
+
   var imagePath = 'img/list/60.jpeg';
 
   $scope.todos = [];
